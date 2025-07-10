d0e0ac00990eafcb197cf6b0fe825c5487d503cc
Scorpion-84 <salimi.ros2005@gmail.com>
2025-07-10 15:21:34 +0330

    Creat controller

---
+++ controller/ticket_controller.py
@@ -0,0 +1,27 @@
+from model.tools.ticket_validation import *
+import tkinter.messagebox as msg
+
+ticket_list = []
+class ticket_controller():
+    
+    def button_save(self, id, ticket_code, origin,
+                 destination,
+                 airline,
+                 start_date_time,
+                 end_date_time,
+                 price,seat_number):
+    
+        try:
+            ticket_ = Ticket(id, ticket_code, origin,
+                    destination,
+                    airline,
+                    start_date_time,
+                    end_date_time,
+                    price,seat_number)
+            ticket_list.append(ticket_)
+            return True, f"Ticket Saved Successfully"
+        except Exception as e:
+            return False, f"Ticket Saved Failed\n{e}"
+        
+#        msg.showinfo(message = 'Saved Successfully')
+#        print('saved successfully')
